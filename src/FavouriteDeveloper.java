9e8c5f1acf96f4e381457fb88dc6a4731ef1e02e
LPL <marc.boss@gmail.com>
2021-07-26 22:09:12 +0200

    MessageFenster :-)

--- src/FavouriteDeveloper.java
+++ src/FavouriteDeveloper.java
@@ -1,5 +1,11 @@
-class FavouriteDeveloper {
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+public class FavouriteDeveloper {
 	public static void main(String[] args) {
+		ImageIcon icon = new ImageIcon("src/bild.jpg");
+		JOptionPane.showMessageDialog(null, "Dily!", "Favourite Developer", JOptionPane.INFORMATION_MESSAGE, icon);
 		System.out.println("Dily!") ;
 	}
+
 }
